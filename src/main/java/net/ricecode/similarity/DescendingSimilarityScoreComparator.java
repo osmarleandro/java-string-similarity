50451a383402c23808b3832992ce857012d9ccf7
Osmar Leandro <osmarleandrods@gmail.com>
2020-03-22 10:58:49 -0300

    ref: Push down method from ExtractedSuperclass#compare() to DescendingSimilarityScoreComparator#compare();

--- src/main/java/net/ricecode/similarity/DescendingSimilarityScoreComparator.java
+++ src/main/java/net/ricecode/similarity/DescendingSimilarityScoreComparator.java
@@ -34,5 +34,24 @@ import java.util.Comparator;
 public class DescendingSimilarityScoreComparator extends ExtractedSuperclass implements Comparator<SimilarityScore>
 {
 
+    /**
+     * Compares two similarity scores.
+     * @param x The first score to be compared.
+     * @param y The second score to be compared.
+     * @return a negative integer, zero, or a positive integer as the first score is greater than,
+     * equal to, or less than the second score.
+     */
+    @Override
+    public int compare(SimilarityScore x, SimilarityScore y) {
+        double first = x.getScore();
+        double second = y.getScore();
+        if (first == second) {
+            return 0;
+        }
+        if (first < second) {
+            return 1;
+        }
+        return -1;
+    }
 }
 
--- src/main/java/net/ricecode/similarity/ExtractedSuperclass.java
+++ src/main/java/net/ricecode/similarity/ExtractedSuperclass.java
@@ -1,23 +1,4 @@
 package net.ricecode.similarity;
 
-public class ExtractedSuperclass implements CompareInterface {
-    /**
-     * Compares two similarity scores.
-     * @param x The first score to be compared.
-     * @param y The second score to be compared.
-     * @return a negative integer, zero, or a positive integer as the first score is greater than,
-     * equal to, or less than the second score.
-     */
-    @Override
-    public int compare(SimilarityScore x, SimilarityScore y) {
-        double first = x.getScore();
-        double second = y.getScore();
-        if (first == second) {
-            return 0;
-        }
-        if (first < second) {
-            return 1;
-        }
-        return -1;
-    }
+public abstract class ExtractedSuperclass implements CompareInterface {
 }
