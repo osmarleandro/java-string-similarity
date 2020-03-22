31ddca42073c97d48a680adcf5b8b043f3fe450d
Osmar Leandro <osmarleandrods@gmail.com>
2020-03-22 10:18:23 -0300

    ref: Pull up method from DescendingSimilarityScoreComparator#compare() to ExtractedSuperclass#compare()

--- src/main/java/net/ricecode/similarity/ExtractedSuperclass.java
+++ src/main/java/net/ricecode/similarity/ExtractedSuperclass.java
@@ -2,10 +2,31 @@ package net.ricecode.similarity;
 
 import java.util.Comparator;
 
+import net.ricecode.SimilarityScore;
+
 public class ExtractedSuperclass {
 
 	public ExtractedSuperclass() {
 		super();
+	}
+
+	/**
+	 * Compares two similarity scores.
+	 * @param x The first score to be compared.
+	 * @param y The second score to be compared.
+	 * @return a negative integer, zero, or a positive integer as the first score is greater than,
+	 * equal to, or less than the second score. 
+	 */
+	public int compare(SimilarityScore x, SimilarityScore y) {
+		double first = x.getScore();
+		double second = y.getScore();
+		if (first == second) {
+			return 0;
+		}
+		if (first < second) {
+			return 1;
+		}
+		return -1;
 	}
 
 }
