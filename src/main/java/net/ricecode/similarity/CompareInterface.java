3e011a21fcd1d9079f90b2769c86923e824191cd
Osmar Leandro <osmarleandrods@gmail.com>
2020-03-22 10:56:35 -0300

    ref: Extract interface CompareInterface at ExtractedSuperclass with compare()

---
+++ src/main/java/net/ricecode/similarity/CompareInterface.java
@@ -0,0 +1,5 @@
+package net.ricecode.similarity;
+
+public interface CompareInterface {
+    int compare(SimilarityScore x, SimilarityScore y);
+}
--- src/main/java/net/ricecode/similarity/ExtractedSuperclass.java
+++ src/main/java/net/ricecode/similarity/ExtractedSuperclass.java
@@ -1,13 +1,15 @@
 package net.ricecode.similarity;
 
-public class ExtractedSuperclass {
+public class ExtractedSuperclass implements CompareInterface {
     /**
      * Compares two similarity scores.
      * @param x The first score to be compared.
      * @param y The second score to be compared.
      * @return a negative integer, zero, or a positive integer as the first score is greater than,
      * equal to, or less than the second score.
-     */public int compare(SimilarityScore x, SimilarityScore y) {
+     */
+    @Override
+    public int compare(SimilarityScore x, SimilarityScore y) {
         double first = x.getScore();
         double second = y.getScore();
         if (first == second) {
